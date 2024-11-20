07ca255a9136071c0a09dd8f9e4a70562d1988bf
G10vann1K0sta <ksivovolov@gmail.com>
2024-11-20 16:52:32 +0800

    feat: add marshmallow validation

--- resources/item.py
+++ resources/item.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import items
+from schemas import ItemSchema, ItemUpdateSchema
 
 
 blp = Blueprint("Items", __name__, description="Operations on items")
@@ -10,22 +11,10 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
-
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "name" not in item_data or "price" not in item_data:
-            abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
-        
-        try:
-            item = items[item_id]
-            item |= item_data
-    
-            return item
         except KeyError:
             abort(404, message="Item not found.")
 
@@ -36,25 +25,30 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found")
 
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
+        # item_data = request.get_json()  # # don't use after adding ItemSchema-validation
+        try:
+            item = items[item_id]
+            item |= item_data
+    
+            return item
+        except KeyError:
+            abort(404, message="Item not found.")
+
+
 
 @blp.route("/item")
 class ItemsList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        # return {"items": list(items.values())}  # don't use after marshmellow-validation
+        return items.values()
 
-    def post(self):
-        item_data = request.get_json()
-
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload."
-            )
-    
+    @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
+    def post(self, item_data):
+        # item_data = request.get_json()  # don't use after adding ItemSchema-validation
         for item in items.values():
             if (
                 item_data["name"] == item["name"]
--- resources/store.py
+++ resources/store.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
+from schemas import StoreSchema
 
 
 blp = Blueprint("stores", __name__, description="Operations on stores.")
@@ -10,6 +11,7 @@ blp = Blueprint("stores", __name__, description="Operations on stores.")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):  # has been took from get_store() due "<string:store_id>"
         try:
             return stores[store_id]
@@ -26,17 +28,13 @@ class Store(MethodView):
 
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(201, StoreSchema(many=True))
     def get(self):
         return {"stores": list(stores.values())}
     
-    def post(self):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'name' is included in the JSON payload."
-            )
-        
+    @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
+    def post(store_data, self):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(
